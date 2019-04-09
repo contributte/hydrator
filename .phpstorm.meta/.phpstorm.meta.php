e3a065c4688c99e7f05e242de5dd8a9addeb8d95
MartkCz <antik494@gmail.com>
2019-04-09 15:36:48 +0200

    add phpstorm.meta

---
+++ .phpstorm.meta/.phpstorm.meta.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PHPSTORM_META;
+
+use Nettrine\Hydrator\IHydrator;
+use Nettrine\Hydrator\Hydrator;
+
+override(IHydrator::toFields(0), type(0));
+override(Hydrator::toFields(0), type(0));
